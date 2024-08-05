6f512a3a9c83cc6546dfb6dda83625fe51112360
Pgodwin1 <pgodwin287@gmail.com>
2024-08-05 21:24:56 +0100

    conncted to mongodb databse

--- src/App.ts
+++ src/App.ts
@@ -1,13 +1,26 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors"
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
-const port = 3400
+const app = express();
+const port = 3400;
 
-app.use(cors())
-app.use(express.json())
+const startServer = async () => {
+  await mongoose
+    .connect(process.env.DATABASE_URL as string)
+    .then(() => console.log("Database connected sucessfully"))
+    .catch((err) => console.log(err));
+    
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    
+    app.listen(port, () => {
+        console.log(`Ecommerce server running on port ${port}`);
+    });
+};
 
-app.listen(port, () => {
-    console.log(`Ecommerce server running on port ${port}`)
-} )
+
+startServer()
